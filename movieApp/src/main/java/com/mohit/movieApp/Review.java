943c10f39e4de64ce1aeb9b2f9158562f606f350
Guptamoheat <guptamohit801@gmail.com>
2023-07-05 13:13:49 +0530

    Review Service and repository completed, backend completed

--- movieApp/src/main/java/com/mohit/movieApp/Review.java
+++ movieApp/src/main/java/com/mohit/movieApp/Review.java
@@ -14,4 +14,10 @@ import lombok.NoArgsConstructor;
 public class Review {
     private ObjectId id;
     private String body;
+    
+    public Review(String body) {
+        this.body = body;
+    }
+
+    
 }
