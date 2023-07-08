be49cbdf2e7f298c5b9a374b5f800d5800b35a18
Guptamoheat <guptamohit801@gmail.com>
2023-07-08 22:24:04 +0530

    Frontend completed and tested succefully

--- movieApp/src/MovieApp/src/App.jsx
+++ movieApp/src/MovieApp/src/App.jsx
@@ -2,13 +2,19 @@ import { useState, useEffect } from 'react'
 import './App.css'
 import api from './assets/api/axiosConfig'
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Layout from './assets/components/Layout'
+import Home from './assets/components/Home/Home'
+import Header from './assets/components/header'
+import Trailer from './assets/components/trailer/Trailer'
+import Reviews from './assets/components/Reviews/Reviews'
 
 function App() {
  
   const [movies, SetMovies] = useState([])
-  //console.log("Component run")
-
   
+  const [movie, setMovie] = useState();
+  const [reviews, setReviews] = useState([]);
   async function getMovies() {
 
     try {
@@ -22,11 +28,28 @@ function App() {
     
   }
 
-  useEffect(() => {getMovies()},[])
+  async function getMovieData(movieId) {
+    const response = await api.get(`/api/v1/movies/${movieId}`)
+    const singleMovie = response.data;
+    setMovie(singleMovie);
+    setReviews(singleMovie.reviewIds)
+    //console.log(reviews)
+    
+  }
+
+  useEffect(() => {getMovies()}, [])
+  //console.log("App component rendered")
   return (
-    <>
-     
-    </>
+    <div className='app'>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Layout/>}>
+          <Route path='/' element={<Home movies={movies}/>}/>
+          <Route path='/Trailer/:ytTrailerId' element={<Trailer/>}></Route>
+          <Route path='/Reviews/:movieId' element={<Reviews getMovieData={getMovieData} reviews={reviews} setReviews={setReviews} movie={movie}/>}/>
+        </Route>
+      </Routes>
+    </div>
   )
 }
 
